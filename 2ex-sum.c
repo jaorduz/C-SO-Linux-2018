bc66432bc76314991e3f54e08bb97ffca8aef8c8
J. A. Orduz-Ducuara <jaorduz@ciencias.unam.mx>
2017-12-11 16:10:19 -0600

    Add files via upload

---
+++ 2ex-sum.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int x;
+
+float y;
+
+float z;
+
+int main(){
+
+printf("Dame el primer numero: ");
+scanf("%i",&x);
+printf("\n");
+printf("Dame el segundo numero: ");
+scanf("%f",&y);
+z=x+y;
+printf("\n\n El resultado de la es suma :%.2f\n",z);
+return 0;
+}
+
+
