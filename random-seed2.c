9ca6a6124446b52e73a7213d02b827e6c5ae6541
J. A. Orduz-Ducuara <jaorduz@ciencias.unam.mx>
2017-12-13 17:31:27 -0600

    Add files via upload

---
+++ random-seed2.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<math.h>
+#include<stdlib.h>
+#include<float.h>
+
+int main(){
+float   x, max, y;
+int     i, cuenta;
+max = RAND_MAX*1.0;
+
+
+srand(1981122);
+
+
+cuenta=0;
+  for(i=1;i<=10;i++){
+x=(rand()/max);
+y=(rand()/max);
+     printf("%i %f %f\n", i, x,y); 
+ }
+return 0;
+}
