634480e554555a4c0f1062afc4b313db40951c33
J. A. Orduz-Ducuara <jaorduz@ciencias.unam.mx>
2017-12-13 17:32:40 -0600

    Add files via upload

---
+++ random-1.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<math.h>
+
+#include<stdlib.h> // necesaria para RAND_MAX que es una contante
+#include<float.h>
+
+int main(){
+
+float  x, max;
+int i;
+
+max = RAND_MAX*1.0;
+
+  for(i=0;i<=10;i++){
+
+     x=(random()/max);
+
+     printf("%i %f\n",i, x); 
+ }
+
+return 0;
+}
