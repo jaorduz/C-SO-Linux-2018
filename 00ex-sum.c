bc7370670e99f494045adc66b9b21a99356b79a1
J. A. Orduz-Ducuara <jaorduz@ciencias.unam.mx>
2017-12-14 08:25:36 -0600

    Update 00ex-sum.c

--- 00ex-sum.c
+++ 00ex-sum.c
@@ -18,4 +18,17 @@ printf("\n\n El resultado de la es suma :%.2f\n",z);
 return 0;
 }
 
+/*
+
+ALGUNAS SUGERENCIAS Y COMENTARIOS QUE DEBES CONSIDERAR
+
+En los scripts (archivos fuente) por lo general no se usan acentos,
+en los nombres no se usan espacios, ni tildes o caracteres extranos.
+
+Implementa documentacion interna o externa.
+
+Algunos archivos fuente tienen errores: 
+compilacion, ejecucion o logicos.
+*/
+
 
